a88923f4065878a956766c5a3381c1e1ed56b61e
ENNEDDI-Omar <enneddiomar@gmail.com>
2025-02-19 19:06:10 +0100

    mise en place du  micro-service Commande

---
+++ server/service-commande/src/main/java/com/projects/servicecommande/ServiceCommandeApplication.java
@@ -0,0 +1,13 @@
+package com.projects.servicecommande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCommandeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCommandeApplication.class, args);
+    }
+
+}
