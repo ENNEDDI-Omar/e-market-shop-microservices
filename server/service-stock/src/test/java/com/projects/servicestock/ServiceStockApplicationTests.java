7bf06918ae3340f5cc6eb69da61051f6ab7f2bd0
ENNEDDI-Omar <enneddiomar@gmail.com>
2025-02-19 19:05:01 +0100

    mise en place du  micro-service Stock

---
+++ server/service-stock/src/test/java/com/projects/servicestock/ServiceStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.servicestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
